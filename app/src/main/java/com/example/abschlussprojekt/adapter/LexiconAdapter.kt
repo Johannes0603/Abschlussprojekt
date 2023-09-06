b8334e15f036cb0468705ff22881ab779013b51c
johannesahlborn <Hannes.9090@gmx.de>
2023-09-06 16:02:50 +0200

    grundaufbau inhalt

--- app/src/main/java/com/example/abschlussprojekt/adapter/LexiconAdapter.kt
+++ app/src/main/java/com/example/abschlussprojekt/adapter/LexiconAdapter.kt
@@ -1,8 +1,29 @@
 package com.example.abschlussprojekt.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.abschlussprojekt.data.model.Plant
+import com.example.abschlussprojekt.databinding.ListItemBinding
+import com.example.abschlussprojekt.ui.ViewModel
 
 class LexiconAdapter (
-    private val dataSet: List<Plant>
-){
+    private val dataSet: List<Plant>, val viewModel: ViewModel,
+): RecyclerView.Adapter<LexiconAdapter.ItemViewHolder>(){
+    inner class ItemViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataSet[position]
+        val binding = holder.binding
+        binding.imageView.setImageURI(null)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
 }
--- app/src/main/java/com/example/abschlussprojekt/ui/ViewModel.kt
+++ app/src/main/java/com/example/abschlussprojekt/ui/ViewModel.kt
@@ -2,5 +2,10 @@ package com.example.abschlussprojekt.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.abschlussprojekt.data.Repository
+import com.example.abschlussprojekt.remote.PlantApi
 
-class ViewModel(application: Application) : AndroidViewModel(application)
+class ViewModel(application: Application) :
+    AndroidViewModel(application)
+val repository = Repository(PlantApi)
+val lexiconList = repository.getPlantList
